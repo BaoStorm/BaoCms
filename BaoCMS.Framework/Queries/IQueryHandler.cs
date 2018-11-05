98df6f93a0e2810f7738fd192d5289517dabb4fe
baoxing <baoxing@31huiyi.com>
2018-11-05 18:08:13 +0800

    Identity

--- BaoCMS.Framework/Queries/IQueryHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BaoCMS.Framework.Queries
-{
-    public interface IQueryHandler<in TQuery, out TResult> where TQuery : IQuery
-    {
-        TResult Retrieve(TQuery query);
-    }
-}
--- BaoCMS.Framework/Queries/IQueryHandlerAsync.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace BaoCMS.Framework.Queries
-{
-    public interface IQueryHandlerAsync<in TQuery, TResult> where TQuery : IQuery
-    {
-        Task<TResult> RetrieveAsync(TQuery query);
-    }
-}
